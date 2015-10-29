bf0518e06f05529361d7593b73d1f180ed58c30f
moe <mariusomoe@gmail.com>
2015-10-29 15:09:02 +0100

    add some lines to buy.js, minor bug fix's

--- site/script/buy.js
+++ site/script/buy.js
@@ -5,10 +5,15 @@
 //-------emulerer hva en database typisk hadde tatt seg av-------
 var basket = []
 , allBooks = ['book1', 'book2', 'book3', 'book4', 'book5', 'book6', 'book7', 'book8' ]
+, allBooksNames = ['sherlock', 'havfruen', 'hesten', 'billa', 'gammeln', 'boka', 'book7', 'boken om alt']
 , books = []
 , bug = true
 , first = true;
 
+/*
+ * get all the buy buttons and add them to a array
+ * hacky solution because ECMAscript is derp and on runtime :(  
+ */
 for (var i = 0; i < allBooks.length; i++) {
 	(function () {
 		try {
@@ -23,7 +28,10 @@ if (bug){
 	console.log(books);
 }
 
-
+/*
+ * not exactly a method :D
+ * add a eventlistener to every button
+ */
 for (var j = 0; j < allBooks.length; j++) {
 	(function (j) {
 		try{
@@ -45,9 +53,18 @@ var stored = getCookie('cbasket');
 if (stored){
 	basket = stored.split('|');
 }
+else{
+	document.getElementById("basketContent").innerHTML = "Din handlekurv er tom";
+}
 
 
 var temp ="";
+/*
+ * adds the selected book to a array containing all the selected books and create a cookie 
+ * containing the same information.
+ * Method also change the text in the selected button 
+ * @param book		the selected book 
+ */
 function addToBasket(book){
 	basket.push(book);
 	
@@ -70,18 +87,31 @@ function addToBasket(book){
 	
 	books[book].innerHTML = "Kjøp en til";
 	
+	var strBooks = "Handlekurv: <br>";
+	for (var u = 0; u < basket.length; u++){
+		
+		strBooks += allBooksNames[basket[u]] + "<br>";
+	}
+	
+	document.getElementById("basketContent").innerHTML = strBooks;
+	//document.getElementById("basketContent").innerHTML = basket[0];
+	
 	
 } 
-
+/*
+ * @return arrSored		a array of the books in the basket 
+ */
 function print(){
-	
-	
 	var storedBasket = getCookie('cbasket');
 	console.log(storedBasket);
 	var arrStored = storedBasket.split('|');
 	console.log("storedBasket: " + arrStored);
+	return arrStored;
 }
 
+/*
+ * clears the cookie with the books ready for purchase and the array containing the same
+ */
 function clearBooks(){
 	basket = [];
 	setCookie('cbasket',"",-1);
@@ -89,7 +119,13 @@ function clearBooks(){
 }
 
 
-
+/*
+ * method is taken from w3s's examples and is free to use.
+ * stores a cookie locally on the clients machine 
+ * @param cname		name of the cookie to be created
+ * @param cvalue	value to put in cookie
+ * @param exdays	days until the cookie will expire (be deleted)
+ */
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
@@ -97,6 +133,11 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + "; " + expires;
 }
 
+/*
+ * method is taken from w3s's examples and is free to use.
+ * retries the cookie created by setCookie()
+ * @param cname		name of the cookie to be retrieved. 
+ */
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
@@ -108,6 +149,8 @@ function getCookie(cname) {
     console.log("Not found");
     return "";
 }
+
+
 
 
 
