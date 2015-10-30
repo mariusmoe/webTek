74727164af6bc353bc5803815f02c97254dd8a40
FlyLice <hungqt@stud.ntnu.no>
2015-10-30 12:37:58 +0100

    slideshow nigga

---
+++ site/script/slideshow.js
@@ -0,0 +1,29 @@
+addEventListener("load", interval);
+
+var imageCounter = 1;
+var total = 3;
+
+function photoSlideshow(){
+	var image = document.getElementById("image");
+	imageCounter++;
+	if(imageCounter > total){
+		imageCounter = 1;
+	}
+	image.src = "../images/slide" + imageCounter + ".jpg";
+}
+
+function photo(i){
+	var image = document.getElementById("image");
+	imageCounter+=i;
+	if(imageCounter > total){
+		imageCounter = 1;
+	}
+	if(imageCounter < 1){
+		imageCounter = total;
+	}
+	image.src = "../images/slide" + imageCounter + ".jpg";
+}
+
+function interval(){
+	window.setInterval(photoSlideshow, 5000);
+}
