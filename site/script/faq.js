1f32d7635dfbc54861e91de1d603b5caf26cdae7
Birathepan Tharmakulasingam <birathet@stud.ntnu.no>
2015-11-06 12:06:27 +0100

    faq done faq.js har fått javadoc

--- site/script/faq.js
+++ site/script/faq.js
@@ -1,9 +1,18 @@
-    console.log("hei");
+   /**
+ * @author Birathepan Tharmakulasingam
+ */
+
+    /*
+ *  Get a list full of boolean elements
+ * its purpose is to make the button hide the answers if open.
+ */
     var liste=[];
         for (var i = 0; i < 9; i++) {
             liste.push(true);
         };
-
+    /*
+ *  function that shows and hides the answer
+ */
         function question1() {
             if (liste[0]==true){
             document.getElementById("questions").innerHTML = "Hvis du ikke får betalt for bøkene er det bare å kontakte oss på support";
@@ -13,7 +22,9 @@
             liste[0]=true;
                     }
             }
-            
+            /*
+ *  function that shows and hides the answer
+ */    
             function question2() {
             if (liste[1]==true){
             document.getElementById("questions2").innerHTML = "Hvis vi mangler noen bøker som dere gjerne ville ha, er det bare å sende melding til oss på support. Vi vil gjøre best mulig for å få tak i bøkene for å fylle kundenes behov";
@@ -23,47 +34,57 @@
             liste[1]=true;
                     }
             }
-
+    /*
+ *  function that shows and hides the answer
+ */
             function question3() {
             if (liste[2]==true){
-            document.getElementById("questions3").innerHTML = "svar";
+            document.getElementById("questions3").innerHTML = "Mandag-Fredag: 8:00- 18:00, Lørdag:9:00-18:00. Stengt under helligdager ";
             liste[2]=false
             }else{
             document.getElementById("questions3").innerHTML = "";
             liste[2]=true;
                     }
             }
-
+    /*
+ *  function that shows and hides the answer
+ */
             function question4() {
             if (liste[3]==true){
-            document.getElementById("questions4").innerHTML = "svar";
+            document.getElementById("questions4").innerHTML = "Det er dessverre ikke mulig å skaffe eierskap i bedriften enda. Sem(vår CEO) har en emosjonell tilknytning til butikken.";
             liste[3]=false
             }else{
             document.getElementById("questions4").innerHTML = "";
             liste[3]=true;
                     }
             }
-
+    /*
+ *  function that shows and hides the answer
+ */
             function question5() {
             if (liste[4]==true){
-            document.getElementById("questions5").innerHTML = "svar";
+            document.getElementById("questions5").innerHTML = "Under dagene vi har bokkvelder, leser vi litt av noen bøker og gir anmeldelse";
             liste[4]=false
             }else{
             document.getElementById("questions5").innerHTML = "";
             liste[4]=true;
                     }
             }
-
+    /*
+ *  function that shows and hides the answer
+ */
             function question6() {
             if (liste[5]==true){
-            document.getElementById("questions6").innerHTML = "svar";
+            document.getElementById("questions6").innerHTML = "Vi søker ikke ansatte for øyeblikket, men det er mulig vi kommer til å øke bemanningen hvis vi skal utvide lokalet vårt ";
             liste[5]=false
             }else{
             document.getElementById("questions6").innerHTML = "";
             liste[5]=true;
                     }
             }
-
+    /*
+ *  function that shows and hides the answer
+ */
             function question7() {
             if (liste[6]==true){
             document.getElementById("questions7").innerHTML = "svar";
@@ -73,7 +94,9 @@
             liste[6]=true;
                     }
             }
-
+    /*
+ *  function that shows and hides the answer
+ */
             function question8() {
             if (liste[7]==true){
             document.getElementById("questions8").innerHTML = "svar";
@@ -83,7 +106,9 @@
             liste[7]=true;
                     }
             }
-
+    /*
+ *  function that shows and hides the answer
+ */
             function question9() {
             if (liste[8]==true){
             document.getElementById("questions9").innerHTML = "svar";
@@ -93,10 +118,12 @@
             liste[8]=true;
                     }
             }
-
+    /*
+ *  function that shows and hides the answer
+ */
             function question10() {
             if (liste[9]==true){
-            document.getElementById("questions10").innerHTML = "svar";
+            document.getElementById("questions10").innerHTML = " Denne siden er en studentoppgave. <br>-'its not about the money, its about sending a message...to student-assistants'";
             liste[9]=false
             }else{
             document.getElementById("questions10").innerHTML = "";
