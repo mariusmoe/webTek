eab773ff40891f64c1e223b50a4c1ddc8af8dbc7
KnasKnas <christian.rudfoss@gmail.com>
2015-11-10 13:58:26 +0100

    map-changes

--- site/script/map.js
+++ site/script/map.js
@@ -1,16 +1,55 @@
 /*Initialiserer kartet*/
+/*
 function initMap() {
-  var myLatLng = {lat: 63.429272, lng: 10.388645};
+  var shop = {lat: 63.429272, lng: 10.388645};
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
-    center: myLatLng
+    center: shop
+
+
   });
   
-/*Lager en marker med hoved-over tittel*/
+//Lager en marker med hoved-over tittel
+
   var marker = new google.maps.Marker({
-    position: myLatLng,
+    position: shop,
     map: map,
     title: "Noah's papir"
   });
+}
+*/
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 63.429272, lng: 10.388645},
+    zoom: 18
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
 }
