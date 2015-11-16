a39bb8934509b800b1481b7214c8bbb93195c708
Sondre Kristensen <sondrkri@stud.ntnu.no>
2015-11-16 10:53:51 +0100

    La til jdocs i size.js, samt laget et document over forbedringer

--- site/script/size.js
+++ site/script/size.js
@@ -1,8 +1,19 @@
+/**
+ *This script toggles an image between two sizes.
+ *@author Sondre Kristensen
+ *@version 1.0
+ *@since 16/11-2015
+ */
+
 var smallwidth = 146;
 var smallheigth = 235;
 var bigwidth = 584;
 var bigheigth = 940;
 
+/**
+*This function takes the id of an image and toggles between sizes in width and height.
+*@param id The id of an element.
+*/
 function toggleScale(id){
 	var element = document.getElementById(id);
 	if (element.width == smallwidth) {
